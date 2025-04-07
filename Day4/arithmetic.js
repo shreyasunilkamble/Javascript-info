93534d9fda3a69c1440f85481991a26c01081946
shreyasunilkamble <shreyakamblebhor@gmail.com>
2025-04-07 18:09:18 +0530

    add console & operators

---
+++ Day4/arithmetic.js
@@ -0,0 +1,25 @@
+let a = 2
+let b = 5
+
+//addition
+
+console.log(a+b)
+
+//substraction
+
+console.log(a-b)
+
+//multiplication
+
+console.log(a*b)
+
+//division
+
+console.log(a/b)
+
+//modulus
+
+let c = 8
+let d = 4
+
+console.log(8 % 4)
---
+++ Day4/script.js
@@ -0,0 +1,15 @@
+let message = "Hellow world"
+
+console.log(message)
+
+let err_message = "something went wrong"
+
+console.error(err_message)
+
+let warn_message = "failed to fetch"
+
+console.warn(warn_message)
+
+let info = "learn js with easycode"
+
+console.info(info)
