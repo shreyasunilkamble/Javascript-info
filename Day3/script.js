fcb130aa9d10c5b70500ea141756349f724b6fd0
shreyasunilkamble <shreyakamblebhor@gmail.com>
2025-04-07 17:41:09 +0530

    we learn data types

---
+++ Day3/script.js
@@ -0,0 +1,39 @@
+//primitive
+
+//Number
+
+let PI = 3.14
+let pincode = 412206
+let radius = 5
+
+
+//string
+
+let college_name = "ATC BHOR"
+let city_name = Pune
+let email_id = "shreyakamblebhor@gmail.com"
+
+//boolean
+//true or false
+
+let LOGINED = true
+let BAN = false
+
+//Null
+ 
+let a = null
+
+//undefined
+
+let b
+
+
+
+//Non-primitive
+
+let obj = {
+    name:"shreya"
+    city:"pune"
+}
+
+let greet = ["hellow", "good", "day"]
